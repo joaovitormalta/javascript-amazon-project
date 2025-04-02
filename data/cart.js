ecc7ef246feddfc189521d025de48addbbb36fbb
João Vitor Malta <126990089+joaovitormalta@users.noreply.github.com>
2025-04-02 11:05:48 -0300

    gerando o html dos pedidos

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = []
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}, {
+  productId: 'aad29d11-ea98-41ee-9285-b916638cac4a',
+  quantity: 2
+}]
 
 export function addToCart(productId) {
   let matchingItem

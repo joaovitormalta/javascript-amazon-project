403b34e012485e216f5e53fc4ccaebd220679856
João Vitor Malta <126990089+joaovitormalta@users.noreply.github.com>
2025-04-02 12:24:08 -0300

    deletando produtos do carrinho

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,9 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 }, {
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
-}, {
-  productId: 'aad29d11-ea98-41ee-9285-b916638cac4a',
-  quantity: 2
 }]
 
 export function addToCart(productId) {
@@ -26,4 +23,16 @@ export function addToCart(productId) {
       quantity: 1
     })
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = []
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem)
+    }
+  })
+
+  cart = newCart
 }

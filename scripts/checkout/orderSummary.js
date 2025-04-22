dccb52cce00eb0723f8f4319f96addd3495f403b
João Vitor Malta <126990089+joaovitormalta@users.noreply.github.com>
2025-04-22 16:24:10 -0300

    Deixando mais interativo

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js"
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = ''
@@ -163,6 +164,7 @@ export function renderOrderSummary() {
         container.remove()
 
         updateCartQuantity()
+        renderPaymentSummary()
       })
     })
 
@@ -174,7 +176,7 @@ export function renderOrderSummary() {
     })
 
     document.querySelector('.js-return-to-home')
-      .innerHTML = `${cartQuantity} items`
+      .innerHTML = cartQuantity === 1 ? "1 item" : `${cartQuantity} items`
   }
 
   updateCartQuantity()
@@ -185,6 +187,7 @@ export function renderOrderSummary() {
         const {productId, deliveryOptionId} = element.dataset
         updateDeliveryOption(productId, deliveryOptionId)
         renderOrderSummary()
+        renderPaymentSummary()
       })
     })
 }
